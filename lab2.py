be5f7183cbd90daaf1f3c0e76e216f4b9b4e0cdf
Calleigh Tanaka <calleightanaka@Calleighs-MBP.attlocal.net>
2025-06-18 17:34:29 -0700

    fixing lab2

---
+++ lab2.py
@@ -0,0 +1,29 @@
+# Lab 2: Calleigh Tanaka
+
+# Step 1: Name variable
+name = "Calleigh Tanaka"
+
+# Step 2: Nickname Variable 
+nickname = "cali"
+
+# Step 3: Age variable
+# int - whole number, float = decimal
+age = 16
+
+# Step 4 BooLean Variable (True or False)
+used_before = False
+
+# Step 5: List Data Structure
+hobbies = ["drawing", "walking", "baking"] 
+
+# Step 6: Dictionary Data Structure (key : value) pairs
+my_favs = { "food" : ["soup" , "fries", "chicken"], "animal" : "orca"}
+
+# Step 7: Set Data Structure
+orange = {"alicia", "melanie", "candy", "cali", "karla", "karla"}
+print(orange)
+
+# Step 8 Tuple Variable
+orange1 = {"alicia", "melanie", "candy", "cali", "karla", "karla"}
+print(orange1)
+print(age)
