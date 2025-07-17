c23886c3d8e367589365e9ff02be093c224b2c02
Calleigh Tanaka <calleightanaka@Calleighs-MBP.attlocal.net>
2025-07-17 12:33:34 -0700

    functions activity

--- functions_activity.py
+++ functions_activity.py
@@ -1,3 +1,5 @@
+#Functions Activity - Calleigh Tanaka
+
 Menu = {"Pizza": 2.99, "Burger": 3.99, "Hot dog": 1.99, "Cheese": 0.59, "Ice cream": 1.49, "Churro": 0.79, "Soda": 0.89}
 
 def total_price (item1, item2) :
